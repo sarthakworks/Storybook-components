06e85268e9067f85ccb5eaf6c4a60f22a657a89b
sarthak bansal <39374452+sarthakworks@users.noreply.github.com>
2024-02-10 13:41:20 +0530

    top nav

--- src/stories/components/topnavbar/sub/TopnavDesktop.jsx
+++ src/stories/components/topnavbar/sub/TopnavDesktop.jsx
@@ -53,8 +53,101 @@ export default function TopnavDesktop() {
           </div>
         </div>
       </div>
-      <div className="topnav-D-L2"></div>
-      <div className="topnav-D-L3"></div>
+      <div className="topnav-D-L2">
+        <div className="L2-container">
+          <div className="L2-left">
+            <nav aria-label="Main Navigation">
+              <ul>
+                <li aria-current="true">
+                  <a href="#" data-navid="personal" aria-current="page">
+                    <span>Products</span>
+                  </a>
+                  <div className="topnav-D-L3">
+                    <div className="L3-container">
+                      <div className="L3-left">
+                        <nav aria-label="Main Navigation">
+                          <ul>
+                            <li class="active" aria-current="true">
+                              <a
+                                href="#"
+                                data-navid="personal"
+                                aria-current="page"
+                              >
+                                <span>Mortgages</span>
+                              </a>
+                            </li>
+                            <li>
+                              <a href="#" data-navid="premier">
+                                <span>Credit Cards</span>
+                              </a>
+                            </li>
+                            <li>
+                              <a href="#" data-navid="business">
+                                <span>Insurance</span>
+                              </a>
+                            </li>
+                          </ul>
+                        </nav>
+                      </div>
+                      <div className="L3-right">
+                        <ul>
+                          <li></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <a href="#" data-navid="premier">
+                    <span>Accounts</span>
+                  </a>
+                  <div className="topnav-D-L3">
+                    <div className="L3-container">
+                      <div className="L3-left">
+                        <nav aria-label="Main Navigation">
+                          <ul>
+                            <li class="active" aria-current="true">
+                              <a
+                                href="#"
+                                data-navid="personal"
+                                aria-current="page"
+                              >
+                                <span>Savings Account</span>
+                              </a>
+                            </li>
+                            <li>
+                              <a href="#" data-navid="premier">
+                                <span>Current Account</span>
+                              </a>
+                            </li>
+                            <li>
+                              <a href="#" data-navid="business">
+                                <span>Account Closure</span>
+                              </a>
+                            </li>
+                          </ul>
+                        </nav>
+                      </div>
+                      <div className="L3-right">
+                        <ul>
+                          <li></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="L2-right">
+            <ul>
+              <li>
+                <input type="search" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
