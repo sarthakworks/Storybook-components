fcb605e0741924257c9c48cb6bc9bb5332593df4
sarthak bansal <39374452+sarthakworks@users.noreply.github.com>
2024-02-10 17:42:04 +0530

    level 4

--- src/stories/components/topnavbar/sub/TopnavDesktop.jsx
+++ src/stories/components/topnavbar/sub/TopnavDesktop.jsx
@@ -78,6 +78,96 @@ export default function TopnavDesktop() {
                               >
                                 <span>Mortgages</span>
                               </a>
+                              <div className="topnav-D-L4">
+                                <div className="L4-container">
+                                  <ul>
+                                    <li>
+                                      <ol>
+                                        <a
+                                          href="javascript:void(0)"
+                                          class="iah__l4-column-title"
+                                          tabindex="-1"
+                                        >
+                                          Mortgages
+                                        </a>
+                                        {[
+                                          "See our mortgage options",
+                                          "First time buyer mortgage",
+                                          "Moving home",
+                                          "Remortgage to NatWest",
+                                          "Buy to let mortgage",
+                                          "Mortgage calculators",
+                                          "Get an Agreement in Principle",
+                                          "Continue your Agreement in Principle",
+                                          "How to apply",
+                                        ].map((text, index) => (
+                                          <li key={index}>
+                                            <a href="#" data-navid="text">
+                                              <span>{text}</span>
+                                            </a>
+                                          </li>
+                                        ))}
+                                      </ol>
+                                    </li>
+                                    <li>
+                                      <ol>
+                                        <a
+                                          href="javascript:void(0)"
+                                          class="iah__l4-column-title"
+                                          tabindex="-1"
+                                        >
+                                          Mortgages
+                                        </a>
+                                        {[
+                                          "See our mortgage options",
+                                          "First time buyer mortgage",
+                                          "Moving home",
+                                          "Remortgage to NatWest",
+                                          "Buy to let mortgage",
+                                          "Mortgage calculators",
+                                          "Get an Agreement in Principle",
+                                          "Continue your Agreement in Principle",
+                                          "How to apply",
+                                        ].map((text, index) => (
+                                          <li key={index}>
+                                            <a href="#" data-navid="text">
+                                              <span>{text}</span>
+                                            </a>
+                                          </li>
+                                        ))}
+                                      </ol>
+                                    </li>
+                                    <li>
+                                      <ol>
+                                        <a
+                                          href="javascript:void(0)"
+                                          class="iah__l4-column-title"
+                                          tabindex="-1"
+                                        >
+                                          Mortgages
+                                        </a>
+                                        {[
+                                          "See our mortgage options",
+                                          "First time buyer mortgage",
+                                          "Moving home",
+                                          "Remortgage to NatWest",
+                                          "Buy to let mortgage",
+                                          "Mortgage calculators",
+                                          "Get an Agreement in Principle",
+                                          "Continue your Agreement in Principle",
+                                          "How to apply",
+                                        ].map((text, index) => (
+                                          <li key={index}>
+                                            <a href="#" data-navid="text">
+                                              <span>{text}</span>
+                                            </a>
+                                          </li>
+                                        ))}
+                                      </ol>
+                                    </li>
+                                  </ul>
+                                </div>
+                              </div>
                             </li>
                             <li>
                               <a href="#" data-navid="premier">
